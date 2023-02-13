3cb5a753376cfdc720d829857fdbfb8a51dcedc9
Owen Lewis <owenalewis@gmail.com>
2023-02-13 16:09:36 +0000

    Imports and displays menuContent

--- src/index.js
+++ src/index.js
@@ -1,61 +1,11 @@
 import "./styles.css";
-import RestaurantImage from "./restaurant_image.jpeg";
+import homeContent from "./home";
+import menuContent from "./menu";
 
-function content() {
-  const contentDiv = document.getElementById("content");
+/*
+PSUEDOCODE
+- Add header element to document with 3 buttons for each home, menu and contact tab
 
-  // Create header div
-  const imageContainer = document.createElement("div");
-  imageContainer.classList.add("image-container");
+*/
 
-  // Add the image to header
-  const myImage = new Image();
-  myImage.src = RestaurantImage;
-  myImage.setAttribute("id", "main-image");
-  myImage.setAttribute("alt", "restaurant-image");
-  imageContainer.appendChild(myImage);
-
-  // Add header to content div
-  contentDiv.appendChild(imageContainer);
-
-  // Create main content div
-  const mainContentDiv = document.createElement("div");
-  mainContentDiv.classList.add("main-content");
-
-  // Add add title to main content div
-  const title = document.createElement("h1");
-  title.classList.add("title");
-  title.textContent = "Odin Restaurant";
-  mainContentDiv.appendChild(title);
-
-  // Add intro paragraphs to main content
-  const para1 = document.createElement("p");
-  para1.classList.add("copy");
-  para1.textContent =
-    "Discover the ultimate dining experience at Odin Restaurant, where authentic cuisine meets modern twist. Our menu features fresh and locally-sourced ingredients, expertly crafted by our team of talented chefs.";
-  mainContentDiv.appendChild(para1);
-
-  const para2 = document.createElement("p");
-  para2.classList.add("copy");
-  para2.textContent =
-    "Whether you're looking for a romantic dinner, night out with friends or a family meal, we have something for everyone.";
-  mainContentDiv.appendChild(para2);
-
-  const para3 = document.createElement("p");
-  para3.classList.add("copy");
-  para3.textContent =
-    "Come taste the difference at our warm and inviting atmosphere.";
-  mainContentDiv.appendChild(para3);
-
-  const para4 = document.createElement("p");
-  para4.classList.add("copy");
-  para4.textContent = "Visit us today.";
-  mainContentDiv.appendChild(para4);
-
-  // Add main content to content div
-  contentDiv.appendChild(mainContentDiv);
-
-  return contentDiv;
-}
-
-document.body.appendChild(content());
+menuContent();
