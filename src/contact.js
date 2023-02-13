46da04a7fd38151af0d8c693b1da976562251f3a
Owen Lewis <owenalewis@gmail.com>
2023-02-13 17:47:01 +0000

    Adds code for appending title

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,23 @@
 const contactContent = () => {
-  console.log("Contact Page");
+  const contentDiv = document.getElementById("content");
+
+  // Create contact container div
+  const contactContainer = document.createElement("div");
+  contactContainer.classList.add("contact-container", "menu");
+
+  // Add contact heading
+  const headerSection = document.createElement("div");
+  headerSection.classList.add("contact-section");
+  const contactHeading = document.createElement("h1");
+  contactHeading.classList.add("title");
+  contactHeading.textContent = "Contact";
+  headerSection.appendChild(contactHeading);
+  contactContainer.appendChild(headerSection);
+
+  // Add contact container div to content div
+  contentDiv.appendChild(contactContainer);
+
+  document.body.appendChild(contentDiv);
 };
 
 export default contactContent;
