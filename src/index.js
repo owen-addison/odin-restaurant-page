736ae5dff02e35e8e829fc217099ce471429a03c
Owen Lewis <owenalewis@gmail.com>
2023-02-13 17:12:28 +0000

    Adds header element with tab buttons Tab button used to clear DOM before calling respective module for new page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import homeContent from "./home";
 import menuContent from "./menu";
+import contactContent from "./contact";
 
 /*
 PSUEDOCODE
@@ -8,4 +9,39 @@ PSUEDOCODE
 
 */
 
-menuContent();
+const clearContent = () => {
+  const currentContent = document.getElementById("content");
+  document.body.removeChild(currentContent);
+  const newContent = document.createElement("div");
+  newContent.setAttribute("id", "content");
+  document.body.appendChild(newContent);
+};
+
+// Add header element top page
+const header = document.createElement("div");
+header.setAttribute("id", "header");
+
+// Add tabs to header
+const tabContainer = document.createElement("div");
+tabContainer.classList.add("tab-container");
+const tabsArray = [
+  { name: "Home", module: homeContent },
+  { name: "Menu", module: menuContent },
+  { name: "Contact", module: contactContent },
+];
+tabsArray.forEach((element) => {
+  const tab = document.createElement("button");
+  tab.classList.add("tab");
+  tab.textContent = element.name;
+  tab.addEventListener("click", () => {
+    clearContent();
+    element.module();
+  });
+  tabContainer.appendChild(tab);
+});
+header.appendChild(tabContainer);
+
+// Append header to document body
+document.body.appendChild(header);
+
+homeContent();
